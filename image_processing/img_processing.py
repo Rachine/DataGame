dfeebb83b733f2aa8dc65976765af4dec1248a0d
Lucas <lucas.thibaut.meyer@gmail.com>
2016-06-28 11:42:33 +0200

    Count the number of edges cuts along the axis

--- image_processing/img_processing.py
+++ image_processing/img_processing.py
@@ -25,16 +25,33 @@ class Image(object):
         self.img = cv2.imread(self.filename)
         self.img_gray = cv2.imread(self.filename, 0)
 
+    @property
+    def size(self):
+        return self.img.shape
+
     def process(self):
         self.laplacian = cv2.Laplacian(self.img_gray, cv2.CV_64F)
-        self.edges = cv2.Canny(self.img_gray, 150, 300)
+        self.edges = cv2.Canny(self.img_gray, 150, 250)
         self.hist = cv2.calcHist([self.img_gray], [0], None, [256], [0, 256])
 
-    def show(self, img=None):
-        if img:
-            plt.imshow(img)
-        else:
-            plt.imshow(self.img)
+    def cut_edges(self, sample=3, f=0.1):
+        """
+        Counts the number of times edges are crossed along the both axis
+        """
+        height = self.size[0]
+        width = self.size[1]
+        width_cuts = 0
+        height_cuts = 0
+        for i in list(np.random.randint(int(height/2*(1-f)), int(height/2*(1+f)), sample)):
+            width_cuts += np.sum(self.edges[i, :]/255)
+        for j in list(np.random.randint(int(width/2*(1-f)), int(width/2*(1+f)), sample)):
+            height_cuts += np.sum(self.edges[:, j]/255)
+        return (width_cuts/sample, height_cuts/sample)
+
+    def show(self, img=None, **kwargs):
+        if img is None:
+            img = self.img
+        plt.imshow(img, **kwargs)
         plt.title(self.filename)
         plt.show()
 
