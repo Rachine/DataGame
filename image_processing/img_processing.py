df4b55e1143b085a347651069132eaa9e2e76b4b
Rachid Riad <riadrachid3@gmail.com>
2016-06-28 11:53:42 -0700

    Inspiration from Lucas branch, process time to write grad and edges

--- image_processing/img_processing.py
+++ image_processing/img_processing.py
@@ -7,77 +7,83 @@ Created on Mon Jun 20 16:48:42 2016
 
 import numpy as np
 import cv2
+from matplotlib import pyplot as plt
+import os
+import re
+import time 
 
-#from matplotlib import pyplot as plt
+# Requires python-qt4 to be installed
+# Used to display correctly images
+plt.switch_backend('Qt4Agg')
 
+IMG_DIR_PATH = "img/"
+IMG_EXT = (r'.*\.jpg',)
+IMG_EXT_RE = [re.compile(pattern) for pattern in IMG_EXT]
+IMG_DIST_GRAD ="grad_img/"
+IMG_DIST_EDGE = "edge_img/"
 
-# Load an color image in grayscale
-img = cv2.imread("img/other.jpg")
-imgNorth = cv2.imread("img/north.jpg")
-imgeqst = cv2.imread("img/east.jpg")
-# Transform image to GrayScale or not
-gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+class Image(object):
+    """
+    The image class manage image processing
+    """
 
-#Gradient transformation 
-laplacian = cv2.Laplacian(gray_image,cv2.CV_64F)
-sobelx = cv2.Sobel(gray_image,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(gray_image,cv2.CV_64F,0,1,ksize=5)
+    def __init__(self, filename):
+        self.filename = IMG_DIR_PATH + filename
+        self.img = cv2.imread(self.filename)
+        self.img_gray = cv2.imread(self.filename, 0)
 
+    @property
+    def size(self):
+        return self.img.shape
 
-#Canny Edge detection check TIVA tp, seems really relevant !!!
-#Hough transormation might give the same insights, TODO check this
-edges = cv2.Canny(gray_image,100,200)
+    def process(self):
+        self.laplacian = cv2.Laplacian(self.img_gray, cv2.CV_64F)
+        self.edges = cv2.Canny(self.img_gray, 150, 250)
+        self.hist = cv2.calcHist([self.img_gray], [0], None, [256], [0, 256])
 
-#Contours and hierarchy might give insights but not same number contour detected
-#Too much preporcessing before use
-#ret,thresh = cv2.threshold(gray_image,127,255,0)
-#contours,hierarchy = cv2.findContours(thresh,2,1)
+    def cut_edges(self, sample=3, f=0.1):
+        """
+        Counts the number of times edges are crossed along the both axis
+        """
+        height = self.size[0]
+        width = self.size[1]
+        width_cuts = 0
+        height_cuts = 0
+        for i in list(np.random.randint(int(height/2*(1-f)), int(height/2*(1+f)), sample)):
+            width_cuts += np.sum(self.edges[i, :]/255)
+        for j in list(np.random.randint(int(width/2*(1-f)), int(width/2*(1+f)), sample)):
+            height_cuts += np.sum(self.edges[:, j]/255)
+        return (width_cuts/sample, height_cuts/sample)
 
-#We can also normalize the histogram of images before detecting features
-hist,bins = np.histogram(img.flatten(),256,[0,256])
+    def show(self, img=None, **kwargs):
+        if img is None:
+            img = self.img
+        plt.imshow(img, **kwargs)
+        plt.title(self.filename)
+        plt.show()
 
-cdf = hist.cumsum()
-cdf_normalized = cdf * hist.max()/ cdf.max()
-
-cdf_m = np.ma.masked_equal(cdf,0)
-cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
-cdf = np.ma.filled(cdf_m,0).astype('uint8')
-
-img2 = cdf[img]
-#Good preprocessing but as in the docs of opencv might loose some orientation
-#For relief so we should so use : Disccus about it
-
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl1 = clahe.apply(gray_image)
-
-#Fourier Transform
-
-
-dft = cv2.dft(np.float32(gray_image),flags = cv2.DFT_COMPLEX_OUTPUT)
-dft_shift = np.fft.fftshift(dft)
-#plt.imshow(magnitude_spectrum, cmap = 'gray')
-magnitude_spectrum = 20*np.log(cv2.magnitude(dft_shift[:,:,0],dft_shift[:,:,1]))
-#Numpy plot not working on my computer 
-#TODO : check manually plotting features for Fourier transformation
-
-
-#I think this is only available on OpenCV 3 I got 2.x
-#sift = cv2.SIFT()
-#kp = sift.detect(gray,None)
-#
-##result_sift=cv2.drawKeypoints(gray_image,kp,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-#sift = cv2.SIFT()
-#kp, des = sift.detectAndCompute(gray_image,None)
-#
+    def output(self, filename):
+        # write the features extracted from the image on the filename
+        pass
 
 
-
-
-#cv2.imshow('image', img2)#
-#cv2.waitKey(0)
-#cv2.destroyAllWindows()
-
-
-#plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-#plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-#plt.show()
+def walk_directory():
+    img_files = []
+    files = os.listdir(IMG_DIR_PATH)
+    for file in files:
+        for pattern in IMG_EXT_RE:
+            if re.match(pattern, file):
+                img_files.append(file)
+                break
+    return img_files
+    
+if __name__ == '__main__':
+    start_time = time.time()
+    img_files = walk_directory()
+    for imgName in img_files:
+        img = Image(imgName)
+        img.process()
+        cv2.imwrite(IMG_DIST_GRAD+ "grad_" + imgName, img.laplacian)
+        cv2.imwrite(IMG_DIST_EDGE+ "edge_" + imgName, img.edges)
+    elapsed_time = time.time() - start_time
+    print elapsed_time
